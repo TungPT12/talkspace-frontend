d07c6dab15edcb0f586c8316cafcff52db9c679d
Noah <noah@innorix.com>
2024-10-10 19:11:24 +0700

    common add useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+export const useLocalStorage = <T>(key: string, initialValue?: T) => {
+  const [localStorage, setNewLocalStorage] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (e) {
+      console.error(`Error setting localStorage key "${key}":`, e)
+      return initialValue
+    }
+  })
+  const setLocalStorage = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore = value instanceof Function ? value(localStorage) : value
+
+      setNewLocalStorage(valueToStore)
+
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
+    } catch (e) {
+      console.error(`Error setting localStorage key "${key}":`, e)
+    }
+  }
+
+  return [localStorage, setLocalStorage]
+}
