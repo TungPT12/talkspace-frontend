2d2f875ff96c03ed2218aff1be23d19b7cd7ffc0
Noah <phvohieunghia@gmail.com>
2024-10-14 23:33:09 +0700

    Squashed commit of the following:
    commit e081a5176f6292fc0ce3d1b04b6473e893036ca2
    Author: Noah <noah@192.168.1.9>
    Date:   Wed Oct 9 21:14:04 2024 +0700
    
        TS-5 test state management
    
    docs: change project name
    
    docs: change project name

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,24 +4,28 @@ import { useTranslation } from 'react-i18next'
 import FourOhFourImage from '../assets/images/404.svg'
 import i18n, { Languages } from '../contexts/i18n'
 import { Modes, useMode } from '../contexts/ModeProvider'
+import { usePersonStore } from '../stores'
 
 export const LoginPage = () => {
   const { t } = useTranslation('test')
   const { mode, setMode } = useMode()
+  const firstName = usePersonStore((state) => state.firstName)
+  const updateFirstName = usePersonStore((state) => state.updateFirstName)
+
   // throw new Error('This component crashes during render')
   const handleChange = () => {
     i18n.changeLanguage(i18n.language === Languages.EN ? Languages.VI : Languages.EN)
   }
 
   return (
-    <div className="relative h-screen w-screen bg-red-600">
+    <div className='relative h-screen w-screen bg-red-600'>
       <img
-        className="absolute h-screen w-screen"
+        className='absolute h-screen w-screen'
         src={
           'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=100'
         }
       />
-      <main className="absolute inset-0">
+      <main className='absolute inset-0'>
         {/* <Modal></Modal> */}
         <div>{t('test2')}</div>
         <button onClick={handleChange}>Change Language</button>
@@ -30,6 +34,19 @@ export const LoginPage = () => {
           <button onClick={() => setMode((v) => (v === Modes.DARK ? Modes.LIGHT : Modes.DARK))}>Change mode</button>
         </div>
         <FourOhFourImage />
+        <label>
+          First name
+          <input
+            // Update the "firstName" state
+            onChange={(e) => updateFirstName(e.currentTarget.value)}
+            value={firstName}
+          />
+        </label>
+
+        <p>
+          Hello, <strong>{firstName}!</strong>
+        </p>
+        {/* <Modal></Modal> */}
       </main>
     </div>
   )
